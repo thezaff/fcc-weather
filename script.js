6fe4accbf73a8a614218da380abd17a35c2ee343
thezaff <zkhodjaev@wiut.uz>
2017-09-03 12:41:50 +0500

    without converter and footer

---
+++ script.js
@@ -0,0 +1,17 @@
+
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        $.get({
+                url: "https://fcc-weather-api.glitch.me/api/current",
+                data: {lat:position.coords.latitude, lon:position.coords.longitude},
+                success: function(json) {
+                    $("#icon").attr('src', json.weather[0].icon);
+                    $("#location").html(json.name + ", " + json.sys.country);
+                    $("#main").html(json.weather[0].main);
+                    $("#temp").html(json.main.temp + "°");
+                    $("#converter").html("C");
+                }
+            });
+        });
+    }
+
