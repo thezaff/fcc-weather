88ff7974d6220f57ca74eb53e6a0bdf0c111527b
thezaff <zkhodjaev@wiut.uz>
2017-09-17 15:22:39 +0500

    done

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-
-  if (navigator.geolocation) {
+var tempunit;
+if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position) {
         $.get({
                 url: "https://fcc-weather-api.glitch.me/api/current",
@@ -8,13 +8,23 @@
                     $("#icon").attr('src', json.weather[0].icon);
                     $("#location").html(json.name + ", " + json.sys.country);
                     $("#main").html(json.weather[0].main);
-                    $("#temp").html(json.main.temp);
-                    $("#converter").html("C");
+                    tempunit = json.main.temp;
+                    $("#temp").html(tempunit);
+                    $("#tempsign").html("°");
+                    $("#celsius").html("C");
+                    $("#farenheit").html("F");
+                    $("#farenheit").on('click', function() {
+                        $("#temp").text((tempunit *  9 / 5) + 32);
+                        $("#celsius").css('color', '#222831');
+                        $("#farenheit").css('color', '#2BB3C0'); 
+                    })
+                    $("#celsius").on('click', function() {
+                        $("#temp").text(json.main.temp);
+                        $("#farenheit").css('color', '#222831');
+                        $("#celsius").css('color', '#2BB3C0');
+                    })  
                 }
             });
         });
     }
-
-function myFunction () {
-    $("#converter").html("F");
-}
+    
